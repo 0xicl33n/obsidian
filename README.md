f9ff8bfb7121a2b84a6b50213f1ba93a68a77292
b0n <ph0sgene@pm.me>
2023-04-14 12:36:50 -0700

    Add readme, deps, config

---
+++ README.md
@@ -0,0 +1,1 @@
+![Obsidian](/assets/logo.png "Obsidian")
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate fstrings;
+use std::fs::File;
+use std::io::{Write, BufReader, BufRead, Error};
+use tui::{backend::CrosstermBackend, Terminal};
+
+fn load_paths() {
+
+}
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
